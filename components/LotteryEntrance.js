96d1c0e072861b8f9f3fb708b9ac4de6d6620896
Chizitere Okey-Awuzie <chizzit45@gmail.com>
2023-09-29 10:34:11 +0100

    next smartcontract

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,111 @@
+// We need a function to enter the lottery
+import { useWeb3Contract } from "react-moralis"
+import { parseArgs } from "util"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
+
+export default function LotteryEntrance() {
+    //We had to go back to the hardhat-smartcontract-lottery-fcc directory and spin up the node using hardhat
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+
+    const dispatch = useNotification()
+
+    //Below is the functio that will be called to enter the lottery
+    //runContractFunction can both send transactions and read state
+    const { runContractFunction: enterRaffle } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, //specify the networkId
+        functionName: "enterRaffle",
+        params: {},
+        msgValue: entranceFee,
+    })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, //specify the networkId
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, //specify the networkId
+        functionName: "getNumberOfPlayers",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, //specify the networkId
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    async function updateUI() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayersFromCall = (await getNumberOfPlayers()).toString()
+        const recentWinnerFromCall = await getRecentWinner()
+        setEntranceFee(entranceFeeFromCall)
+        setNumPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
+    }
+
+    //we are going to use one of our hooks. useEffect. useEffect can run when something changes.
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            //try to read the raffle entrance fee
+
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNotification(tx)
+        updateUI()
+    }
+
+    const handleNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+    return (
+        <div>
+            Hi from Lottery Entrance!
+            {raffleAddress ? (
+                <div>
+                    <button
+                        onClick={async function () {
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
+                        }}
+                    >
+                        Enter Raffle
+                    </button>
+                    Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH Players:{" "}
+                    {numPlayers}
+                    Recent Winner: {recentWinner}
+                </div>
+            ) : (
+                <div>No Raffle Address Detected</div>
+            )}
+        </div>
+    )
+}
---
+++ constants/index.js
@@ -0,0 +1,9 @@
+//This file was created to export the abiand the contract addresses in the constants folder.
+
+const contractAddresses = require("./contractAddresses.json")
+const abi = require("./abi.json")
+
+module.exports = {
+    abi,
+    contractAddresses,
+}
